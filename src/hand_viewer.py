14eca36c880c383e2626397ba0222671c8f19dd0
JCampbell9 <campbjos@oregonstate.edu>
2022-05-16 18:02:30 -0700

    created the forward kinematics from the urdf

---
+++ src/hand_viewer.py
@@ -0,0 +1,109 @@
+"""Script for testing the generated robot manipulators in a simply pybullet enviroment."""
+
+# Author: Josh Campbell, campbjos@oregonstate.edu
+# Date: 3-14-2022
+
+#!/usr/bin/python3
+
+import pybullet as p
+import time
+import pybullet_data
+import os
+import json
+import glob
+from math import pi
+
+
+class sim_tester():
+    """Simulator class to test different hands in."""
+
+    def __init__(self, gripper_name, gripper_loc):
+        """Initialize the sim_tester class.
+
+        Args:
+            gripper_name (str): The name of the gripper to be pulled into the simulator enviroment
+            gripper_loc (str): The location of the top hand directory in the output directory
+        """
+        self.gripper_name = gripper_name
+        self.gripper_loc = gripper_loc
+        
+        self.directory = os.path.dirname(__file__)
+
+
+    def main(self):
+        """Run the simulator."""           
+        physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
+        p.setGravity(0, 0, -10)
+        LinkId = []
+        cubeStartPos = [0, 0, 1]
+        cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
+
+        boxId = p.loadURDF(f"{self.gripper_loc}/{self.gripper_name}.urdf", useFixedBase=1)#, baseOrientation=p.getQuaternionFromEuler([0, pi/2, pi/2]))
+
+        gripper = boxId
+
+        p.resetDebugVisualizerCamera(cameraDistance=.2, cameraYaw=180, cameraPitch=-91, cameraTargetPosition=[0, 0.1, 0.1])
+
+        for i in range(0, p.getNumJoints(gripper)):
+            
+            p.setJointMotorControl2(gripper, i, p.POSITION_CONTROL, targetPosition=0, force=0)
+            linkName = p.getJointInfo(gripper, i)[12].decode("ascii")
+            if "sensor" in linkName:
+                LinkId.append("skip")
+            else:
+                LinkId.append(p.addUserDebugParameter(linkName, -3.14, 3.14, 0))
+
+
+
+        while p.isConnected():
+
+            p.stepSimulation()
+            time.sleep(1. / 240.)
+
+            for i in range(0, len(LinkId)):
+                if LinkId[i] != "skip":
+                    linkPos = p.readUserDebugParameter(LinkId[i])
+                    p.setJointMotorControl2(gripper, i, p.POSITION_CONTROL, targetPosition=linkPos)
+
+
+        p.disconnect()
+    
+def read_json(file_loc):
+    """Read contents of a given json file.
+
+    Args:
+        file_loc (str): Full path to the json file including the file name.
+
+    Returns:
+        dictionary: dictionary that contains the content from the json.
+    """
+    with open(file_loc, "r") as read_file:
+        file_contents = json.load(read_file)
+    return file_contents
+
+
+if __name__ == '__main__':
+
+    directory = os.getcwd()
+
+    # file_content = read_json("./../src/.user_info.json")
+    folders = []
+    hand_names = []
+    for folder in glob.glob(f'./resources/*/'):
+        folders.append(folder)
+
+
+    for i, hand in enumerate(folders):
+        temp_hand = hand.split('/')
+        hand_names.append(temp_hand[-2])
+        print(f'{i}:   {temp_hand[-2]}')
+
+    input_num = input("Enter the number of the hand you want loaded:   ")
+    num = int(input_num)
+
+    hand_name = hand_names[num]
+    hand_loc = folders[num]
+
+    sim_test = sim_tester(hand_name, hand_loc)
+    sim_test.main()
--- src/urdf_to_kinematic_chain.py
+++ src/urdf_to_kinematic_chain.py
@@ -1,13 +1,129 @@
 #!/usr/bin/python3
 
+import re
+import xmltodict
+import pprint
+import numpy as np
+from math import sin, cos, pi
+
 
 class UrdfToKinematicChain():
 
     def __init__(self, urdf_file) -> None:
-        pass
+        
+        robot_urdf = self.read_urdf(urdf_file=urdf_file)
+        # pprint.pprint(robot_urdf['joint'][0], indent=2)
+        number_of_joints = len(robot_urdf['joint'])
+        self.joint_angles = [0]*number_of_joints
+        # print(self.joint_angles)
+        self.kinematic_chain = self.intial_kinematic_setup(robot_urdf['joint'])
+        # pprint.pprint(self.kinematic_chain, indent=2)
+        self.transformation_matrix = {}
+
+        self.calculate_forward_kinematics()
+        # pprint.pprint(self.transformation_matrix)
+                 
+    def update_joint_angles(self, angles:list):
+        self.joint_angles = angles
+
+    def calculate_forward_kinematics(self, joint_angles=None):
+        if joint_angles == None:
+            joint_angles = self.joint_angles
+        
+        joint_iterator = 0
+
+        for i in range(len(self.kinematic_chain.keys())):
+            finger_chain = self.kinematic_chain[f'finger{i}']
+            
+            previouse_transform = np.identity(4)
+            for joint in finger_chain:
+                trans = self.translation_matrix(joint['translation'])
+                orient = self.rotation_matrix(rotation_axis=joint['orientation'])
+                joint_rot = self.rotation_matrix(rotation_axis= joint['joint_axis'], angle=joint_angles[joint_iterator])
+                previouse_transform = np.matmul(previouse_transform, np.matmul(trans, np.matmul(orient, joint_rot)))
 
 
-    def read_urdf(self, urdf_file):
-        pass
+                joint_iterator += 1
 
-    
+            self.transformation_matrix[f'finger{i}'] = previouse_transform
+
+    def intial_kinematic_setup(self, joint_list):
+        kinematic_chain = {}
+        
+        for i, joint in enumerate(joint_list):
+            finger, segment, _ = joint['@name'].split('_')
+
+            translation = joint['origin']['@xyz'].split(' ')
+            orientation = joint['origin']['@rpy'].split(' ')
+            joint_axis = joint['axis']['@xyz'].split(' ')
+            joint_type = joint['@type']
+            child_link = joint['child']['@link']
+            try:
+                kinematic_chain[finger].append({"joint_number": segment[-1],
+                                                "translation":translation, 
+                                                "orientation":orientation, 
+                                                "joint_axis":joint_axis,
+                                                "joint_type":joint_type,
+                                                "child_link": child_link})
+            except:
+                kinematic_chain[finger] = [{"joint_number": segment[-1],
+                                            "translation":translation, 
+                                            "orientation":orientation, 
+                                            "joint_axis":joint_axis,
+                                            "joint_type":joint_type,
+                                            "child_link": child_link}]
+        return kinematic_chain
+
+
+    def rotation_matrix(self, rotation_axis:list, angle=None):
+
+        rot_mat = np.identity(4)
+
+        if angle == None:
+            temp_vec = np.array(rotation_axis, dtype=float)
+            angle = np.linalg.norm(temp_vec)
+
+            if angle == 0: # no rotation return identity matrix
+                return rot_mat
+            # print(f'incoming vector: {rotation_axis}, temp_vec: {temp_vec}, angle: {angle}')
+            rotation_axis = temp_vec / angle
+        # else:
+            # print(f'incoming vector: {rotation_axis}, angle: {angle}')
+        
+        if rotation_axis[0] == 1:
+            rot_mat[:3, :3] = np.array([[1, 0, 0],
+                                        [0, cos(angle), -sin(angle)],
+                                        [0, sin(angle), cos(angle)]])
+        elif rotation_axis[1] == 1:
+            rot_mat[:3, :3] = np.array([
+                                        [cos(angle), 0, sin(angle)],
+                                        [0, 1, 0]
+                                        [-sin(angle), 0, cos(angle)]])
+        elif rotation_axis[2] == 1:
+            rot_mat[:3, :3] = np.array([[cos(angle), -sin(angle), 0],
+                                        [sin(angle), cos(angle), 0],
+                                        [0, 0, 1]])
+
+        return rot_mat
+
+    def translation_matrix(self, translation=list):
+        trans_mat = np.identity(4)
+        trans_mat[:3, 3] = translation
+        # print(trans_mat)
+        return trans_mat
+        
+
+    def read_urdf(self, urdf_file="./resources/test_hand/test_hand.urdf"):
+        with open(urdf_file) as f:
+            urdf = xmltodict.parse(f.read())
+        
+        return urdf['robot']
+
+
+
+
+if __name__ == '__main__':
+
+    kinematics = UrdfToKinematicChain("./resources/test_hand/test_hand.urdf")
+    results = np.matmul(kinematics.transformation_matrix['finger0'], np.array([1, 0, 0, 1]))
+    print(results)
